f60f8682d2389e0d62a879a794b278ad245ab463
wenyan2 <150924482+wenyan2@users.noreply.github.com>
2025-01-17 13:32:12 +0100

    Adding GSAP files

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// GSAP Modules
+gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
+
+// GSAP Demo Animation
+gsap.to("#demo",{
+    rotation : 360,
+    duration : 2,
+})
+
+// gsap.fromTO("#demo",{
+//     rotation : 180
+// },{
+//     rotation : 360,
+//     duration : 2,
+// })
